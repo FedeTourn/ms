df8e3945fdc2e4e705f6414f569cc94baa820b78
fedeTourn <fede450@outlook.com>
2022-11-01 23:55:07 -0300

    Crea Metodo diferencias divididas -El metodo sirve para obtener los coeficientes de los terminos del polinomio que queramos calcular -Falta crear el polinomio con los datos obtenidos

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from matplotlib import pyplot as plt
-
 from datos import Datos as d
 
-x, y = d.medido()
+# x, y = d.medido()
 
-plt.plot(x,y)
-plt.show()
+# plt.plot(x,y)
+# plt.show()
 
 x, y = d.modelo()
 
-plt.plot(x,y)
-plt.show()
+# plt.plot(x,y)
+# plt.show()
+
+def diferenciasDivididas(x,y):
+    dD = []
+    tIndep = []
+    tIndep.append(y[0])
+    
+    for j in range(len(x)-1):
+        if(j == 0):
+            for i in range(len(x)-1):
+                valor = (y[i+1]-y[i])/(x[i+1]-x[i])
+                dD.append(valor)    
+        else:
+            dD1=[]
+            for i in range(len(dD)-1):
+                valor = (dD[i+1]-dD[i])/(x[i+j+1]-x[i])
+                
+                dD1.append(valor)
+            dD = dD1
+        tIndep.append(dD[0])
+    #print(tIndep)
+    return tIndep
+
+terminosIndep = diferenciasDivididas(x,y)
